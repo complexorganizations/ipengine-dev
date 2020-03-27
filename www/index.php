9697742626a8558ea4e04e287af57eb1e30601a7
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-03-27 00:34:45 -0400

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,23 +1,18 @@
 <?php
+//whether ip is from share internet
+if (!empty($_SERVER['HTTP_CLIENT_IP']))   
+  {
+    $ip_address = $_SERVER['HTTP_CLIENT_IP'];
+  }
+//whether ip is from proxy
+elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))  
+  {
+    $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
+  }
+//whether ip is from remote address
+else
   {
     $ip_address = $_SERVER['REMOTE_ADDR'];
-    $user_agent = $_SERVER['HTTP_USER_AGENT'];
-    $host_name = $_SERVER['REMOTE_HOST'];
-    $remote_port = $_SERVER['REMOTE_PORT'];
-    $http_accept = $_SERVER['HTTP_ACCEPT'];
-    $http_host = $_SERVER['HTTP_HOST'];
-    $http_connection = $_SERVER['HTTP_CONNECTION'];
-    $http_language = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
   }
-echo nl2br("{
-    IP=$ip_address
-    HOST_NAME=$host_name
-    REMOTE_PORT=$remote_port
-    USER_AGENT=$user_agent
-    CITY=$user_city
-    HTTP_ACCEPT=$http_accept
-    HTTP_HOST=$http_host
-    HTTP_CONNECTION=$http_connection
-    HTTP_LANGUAGE=$http_language
-}");
+echo $ip_address;
 ?>
