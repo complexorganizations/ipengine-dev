364e003d206fa7345c51a2bdb3f360ffe5601a79
zunnorain <zunnorainzaheer360@gmail.com>
2020-04-01 14:54:56 +0500

    network interfaces issue resolved

--- main.go
+++ main.go
@@ -7,6 +7,15 @@ import (
 	"strings"
 )
 
+type Interface struct {
+	Index        int    // positive integer that starts at one, zero is never used
+	MTU          int    // maximum transmission unit
+	Name         string // e.g., "en0", "lo0", "eth0.100"
+	HardwareAddr string // IEEE MAC-48, EUI-48 and EUI-64 form
+	Flags        string
+	Addr         []string // e.g., FlagUp, FlagLoopback, FlagMulticast
+}
+
 func main() {
 	http.HandleFunc("/", ExampleHandler)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
@@ -28,7 +37,7 @@ func ExampleHandler(w http.ResponseWriter, r *http.Request) {
 	if err == nil {
 		resp["hostname"] = hostname[0]
 	}
-
+	resp["interface"] = GetInterfaces()
 	b, _ := json.Marshal(resp)
 
 	_, _ = w.Write(b)
@@ -47,4 +56,26 @@ func GetIP(r *http.Request) string {
 func GetHostName(ip string) ([]string, error) {
 	host, err := net.LookupAddr(ip)
 	return host, err
+}
+func GetInterfaces() []Interface {
+
+	var ifaces []Interface
+	interfaces, _ := net.Interfaces()
+	for _, inter := range interfaces {
+		addrs, _ := inter.Addrs()
+		var address []string
+		for _, ipaddr := range addrs {
+			address = append(address, ipaddr.String())
+		}
+		iface := Interface{
+			Index:        inter.Index,
+			MTU:          inter.MTU,
+			Name:         inter.Name,
+			HardwareAddr: inter.HardwareAddr.String(),
+			Flags:        inter.Flags.String(),
+			Addr:         address,
+		}
+		ifaces = append(ifaces, iface)
+	}
+	return ifaces
 }
