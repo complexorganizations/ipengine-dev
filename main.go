6c1b656f4cb201b1fd42b69fd8c76e7ea21bad56
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-04-01 12:42:03 -0400

    A-IM

--- main.go
+++ main.go
@@ -26,6 +26,7 @@ func ExampleHandler(w http.ResponseWriter, r *http.Request) {
 		"user_agent": r.Header.Get("User-Agent"),
 		"accept_language": r.Header.Get("Accept-Language"),
 		"accept_encoding": r.Header.Get("Accept-Encoding"),
+		"a_im": r.Header.Get("A-IM"),
 	}
 	hostname, err := GetHostName(ip)
 	if err == nil {
