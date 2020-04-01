0f5690e9db90a1c2a6f4c3f8f823f5ef9cf8ad0c
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-04-01 17:24:04 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,29 +1,33 @@
 package main
 
 import (
-        "encoding/json"
-        "net/http"
+	"encoding/json"
+	"net/http"
 )
 
 func main() {
-        http.HandleFunc("/", ExampleHandler)
-        if err := http.ListenAndServe(":8080", nil); err != nil {
-                panic(err)
-        }
+	http.HandleFunc("/", ExampleHandler)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
 }
 
 func ExampleHandler(w http.ResponseWriter, r *http.Request) {
-        w.Header().Add("Content-Type", "application/json")
-        resp, _ := json.Marshal(map[string]string{
-                "ip":                        r.Header.Get("CF-CONNECTING-IP"),
-                "user_agent":                r.Header.Get("User-Agent"),
-                "accept_language":           r.Header.Get("Accept-Language"),
-                "accept_encoding":           r.Header.Get("Accept-Encoding"),
-                "accept":                    r.Header.Get("Accept"),
-                "referer":                   r.Header.Get("Referer"),
-                "upgrade_insecure_requests": r.Header.Get("Upgrade-Insecure-Requests"),
-                "cache_control":             r.Header.Get("Cache-Control"),
-                "sec_fetch_site":            r.Header.Get("Sec-Fetch-Site"),
-        })
-        w.Write(resp)
+	w.Header().Add("Content-Type", "application/json")
+	resp, _ := json.Marshal(map[string]string{
+		"accept":                    r.Header.Get("Accept"),
+		"accept_encoding":           r.Header.Get("Accept-Encoding"),
+		"accept_language":           r.Header.Get("Accept-Language"),
+		"cache_control":             r.Header.Get("Cache-Control"),
+		"dnt":                       r.Header.Get("DNT"),
+		"ip":                        r.Header.Get("CF-CONNECTING-IP"),
+		"referer":                   r.Header.Get("Referer"),
+		"sec_fetch_dest":            r.Header.Get("Sec-Fetch-Dest"),
+		"sec_fetch_mode":            r.Header.Get("Sec-Fetch-Mode"),
+		"sec_fetch_user":            r.Header.Get("Sec-Fetch-User"),
+		"sec_fetch_site":            r.Header.Get("Sec-Fetch-Site"),
+		"upgrade_insecure_requests": r.Header.Get("Upgrade-Insecure-Requests"),
+		"user_agent":                r.Header.Get("User-Agent"),
+	})
+	w.Write(resp)
 }
