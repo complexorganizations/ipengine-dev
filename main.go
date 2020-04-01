25a36ead5fe05c054b9f8a501d971917165a2269
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-04-01 16:21:05 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,58 +1,29 @@
 package main
 
 import (
-	"encoding/json"
-	"net"
-	"net/http"
-	"strings"
+        "encoding/json"
+        "net/http"
 )
 
 func main() {
-	http.HandleFunc("/", ExampleHandler)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		panic(err)
-	}
+        http.HandleFunc("/", ExampleHandler)
+        if err := http.ListenAndServe(":8080", nil); err != nil {
+                panic(err)
+        }
 }
 
 func ExampleHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "application/json")
-
-	addr := GetIP(r)
-	addrList := strings.Split(addr, ":")
-	ip := addrList[0]
-
-	resp := map[string]interface{}{
-		"ip":                        GetIP(r),
-		"user_agent":                r.Header.Get("User-Agent"),
-		"accept_language":           r.Header.Get("Accept-Language"),
-		"accept_encoding":           r.Header.Get("Accept-Encoding"),
-		"accept":                    r.Header.Get("Accept"),
-		"referer":                   r.Header.Get("Referer"),
-		"upgrade_insecure_requests": r.Header.Get("Upgrade-Insecure-Requests"),
-		"cache_control":             r.Header.Get("Cache-Control"),
-		"sec_fetch_site":            r.Header.Get("Sec-Fetch-Site"),
-	}
-	hostname, err := GetHostName(ip)
-	if err == nil {
-		resp["hostname"] = hostname[0]
-	}
-
-	b, _ := json.Marshal(resp)
-
-	_, _ = w.Write(b)
-}
-
-// GetIP gets a requests IP address by reading off the forwarded-for
-// header (for proxies) and falls back to use the remote address.
-func GetIP(r *http.Request) string {
-	forwarded := r.Header.Get("CF-CONNECTING-IP")
-	if forwarded != "" {
-		return forwarded
-	}
-	return r.RemoteAddr
-}
-
-func GetHostName(ip string) ([]string, error) {
-	host, err := net.LookupAddr(ip)
-	return host, err
+        w.Header().Add("Content-Type", "application/json")
+        resp, _ := json.Marshal(map[string]string{
+                "ip":                        r.Header.Get("CF-CONNECTING-IP"),
+                "user_agent":                r.Header.Get("User-Agent"),
+                "accept_language":           r.Header.Get("Accept-Language"),
+                "accept_encoding":           r.Header.Get("Accept-Encoding"),
+                "accept":                    r.Header.Get("Accept"),
+                "referer":                   r.Header.Get("Referer"),
+                "upgrade_insecure_requests": r.Header.Get("Upgrade-Insecure-Requests"),
+                "cache_control":             r.Header.Get("Cache-Control"),
+                "sec_fetch_site":            r.Header.Get("Sec-Fetch-Site"),
+        })
+        w.Write(resp)
 }
