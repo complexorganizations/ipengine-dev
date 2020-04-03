93fa227ff50071b89445850d59633f9de3cafa85
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-04-03 18:21:57 -0400

    Update main.go

--- main.go
+++ main.go
@@ -14,27 +14,33 @@ func main() {
 }
 
 func ExampleHandler(w http.ResponseWriter, r *http.Request) {
+	niler := func(v string) interface{} {
+		if v == "" {
+			return nil
+		}
+		return v
+	}
 	w.Header().Add("Content-Type", "application/json")
 
 	hostname := GetHostName(r.Header.Get("CF-CONNECTING-IP"))
 	reverseIp := GetReverseIp(hostname)
 
 	resp, _ := json.MarshalIndent(map[string]interface{}{
-		"accept":                    r.Header.Get("Accept"),
-		"accept_encoding":           r.Header.Get("Accept-Encoding"),
-		"accept_language":           r.Header.Get("Accept-Language"),
-		"cache_control":             r.Header.Get("Cache-Control"),
-		"dnt":                       r.Header.Get("DNT"),
-		"ip":                        r.Header.Get("CF-CONNECTING-IP"),
-		"hostname":                  hostname,
-		"reverse_hostname":          reverseIp,
-		"referer":                   r.Header.Get("Referer"),
-		"sec_fetch_dest":            r.Header.Get("Sec-Fetch-Dest"),
-		"sec_fetch_mode":            r.Header.Get("Sec-Fetch-Mode"),
-		"sec_fetch_user":            r.Header.Get("Sec-Fetch-User"),
-		"sec_fetch_site":            r.Header.Get("Sec-Fetch-Site"),
-		"upgrade_insecure_requests": r.Header.Get("Upgrade-Insecure-Requests"),
-		"user_agent":                r.Header.Get("User-Agent"),
+		"accept":                    niler(r.Header.Get("Accept")),
+		"accept_encoding":           niler(r.Header.Get("Accept-Encoding")),
+		"accept_language":           niler(r.Header.Get("Accept-Language")),
+		"cache_control":             niler(r.Header.Get("Cache-Control")),
+		"dnt":                       niler(r.Header.Get("DNT")),
+		"ip":                        niler(r.Header.Get("CF-CONNECTING-IP")),
+		"hostname":                  niler(hostname),
+		"reverse_hostname":          niler(reverseIp),
+		"referer":                   niler(r.Header.Get("Referer")),
+		"sec_fetch_dest":            niler(r.Header.Get("Sec-Fetch-Dest")),
+		"sec_fetch_mode":            niler(r.Header.Get("Sec-Fetch-Mode")),
+		"sec_fetch_user":            niler(r.Header.Get("Sec-Fetch-User")),
+		"sec_fetch_site":            niler(r.Header.Get("Sec-Fetch-Site")),
+		"upgrade_insecure_requests": niler(r.Header.Get("Upgrade-Insecure-Requests")),
+		"user_agent":                niler(r.Header.Get("User-Agent")),
 	}, "", "  ")
 
 	w.Write(resp)
