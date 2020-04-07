a7beb5e102d1d66b7588c9106cb91462fc0d565a
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-04-07 19:28:11 -0400

    Update main.go

--- api/main.go
+++ api/main.go
@@ -26,20 +26,9 @@ func ExampleHandler(w http.ResponseWriter, r *http.Request) {
 	reverseIp := GetReverseIp(hostname)
 
 	resp, _ := json.MarshalIndent(map[string]interface{}{
-		"accept":                    niler(r.Header.Get("Accept")),
-		"accept_encoding":           niler(r.Header.Get("Accept-Encoding")),
-		"accept_language":           niler(r.Header.Get("Accept-Language")),
-		"cache_control":             niler(r.Header.Get("Cache-Control")),
-		"dnt":                       niler(r.Header.Get("DNT")),
 		"ip":                        niler(r.Header.Get("X-Forwarded-For")),
 		"hostname":                  niler(hostname),
 		"reverse_hostname":          niler(reverseIp),
-		"referer":                   niler(r.Header.Get("Referer")),
-		"sec_fetch_dest":            niler(r.Header.Get("Sec-Fetch-Dest")),
-		"sec_fetch_mode":            niler(r.Header.Get("Sec-Fetch-Mode")),
-		"sec_fetch_user":            niler(r.Header.Get("Sec-Fetch-User")),
-		"sec_fetch_site":            niler(r.Header.Get("Sec-Fetch-Site")),
-		"upgrade_insecure_requests": niler(r.Header.Get("Upgrade-Insecure-Requests")),
 		"user_agent":                niler(r.Header.Get("User-Agent")),
 	}, "", "  ")
 
