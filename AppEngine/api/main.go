91e439e78b614b0e0ca39350185c8ca2e06d9ccf
Prajwal Koirala <prajwalkoirala23@gmail.com>
2020-04-09 17:39:47 -0400

    revert
    Signed-off-by: Prajwal Koirala <prajwalkoirala23@gmail.com>

--- AppEngine/api/main.go
+++ AppEngine/api/main.go
@@ -1,16 +1,52 @@
 package main
 
-import log "github.com/ermanimer/logger"
+import (
+	"encoding/json"
+	"net"
+	"net/http"
+)
 
 func main() {
-	//logger
-	log.Initialize("logs", log.DebugTraceLevel)
-	//configuration
-	c, err := getConfiguration()
-	if err != nil {
-		log.Fatal(err.Error())
+	http.HandleFunc("/", ExampleHandler)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
 	}
-	//http server
-	err = startHttpServer(c)
-	log.Fatal(err.Error())
+}
+
+func ExampleHandler(w http.ResponseWriter, r *http.Request) {
+	niler := func(v string) interface{} {
+		if v == "" {
+			return "null"
+		}
+		return v
+	}
+	w.Header().Add("Content-Type", "application/json")
+
+	hostname := GetHostName(r.Header.Get("X-Forwarded-For"))
+	reverseIp := GetReverseIp(hostname)
+
+	resp, _ := json.MarshalIndent(map[string]interface{}{
+		"ip":                        niler(r.Header.Get("X-Forwarded-For")),
+		"hostname":                  niler(hostname),
+		"reverse_hostname":          niler(reverseIp),
+		"user_agent":                niler(r.Header.Get("User-Agent")),
+	}, "", "  ")
+
+	w.Write(resp)
+}
+
+func GetHostName(ip string) string {
+	host, err := net.LookupAddr(ip)
+	if err != nil {
+		return ""
+	}
+	return host[0]
+}
+
+func GetReverseIp(host string) string {
+	addr, err := net.LookupIP(host)
+	if err == nil {
+		return addr[0].String()
+	}
+	return ""
 }
