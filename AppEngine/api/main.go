56abaad29679e2b70d5a74d54badf0474e296f5e
ermanimer <ermanimer@gmail.com>
2020-04-10 00:03:57 +0300

    API returns ip address, hostname and reverse ip address

--- AppEngine/api/main.go
+++ AppEngine/api/main.go
@@ -1,52 +1,16 @@
 package main
 
-import (
-	"encoding/json"
-	"net"
-	"net/http"
-)
+import log "github.com/ermanimer/logger"
 
 func main() {
-	http.HandleFunc("/", ExampleHandler)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		panic(err)
-	}
-}
-
-func ExampleHandler(w http.ResponseWriter, r *http.Request) {
-	niler := func(v string) interface{} {
-		if v == "" {
-			return "null"
-		}
-		return v
-	}
-	w.Header().Add("Content-Type", "application/json")
-
-	hostname := GetHostName(r.Header.Get("X-Forwarded-For"))
-	reverseIp := GetReverseIp(hostname)
-
-	resp, _ := json.MarshalIndent(map[string]interface{}{
-		"ip":                        niler(r.Header.Get("X-Forwarded-For")),
-		"hostname":                  niler(hostname),
-		"reverse_hostname":          niler(reverseIp),
-		"user_agent":                niler(r.Header.Get("User-Agent")),
-	}, "", "  ")
-
-	w.Write(resp)
-}
-
-func GetHostName(ip string) string {
-	host, err := net.LookupAddr(ip)
+	//logger
+	log.Initialize("logs", log.DebugTraceLevel)
+	//configuration
+	c, err := getConfiguration()
 	if err != nil {
-		return ""
+		log.Fatal(err.Error())
 	}
-	return host[0]
-}
-
-func GetReverseIp(host string) string {
-	addr, err := net.LookupIP(host)
-	if err == nil {
-		return addr[0].String()
-	}
-	return ""
+	//http server
+	err = startHttpServer(c)
+	log.Fatal(err.Error())
 }
