2f42c0440d565ad39c994bc4120cfb1c64e5b36d
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-04-11 09:30:06 -0400

    Update main.go

--- AppEngine/api/main.go
+++ AppEngine/api/main.go
@@ -2,51 +2,195 @@ package main
 
 import (
 	"encoding/json"
-	"net"
+	"errors"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"strings"
+	"time"
 )
 
+type ArinRdapData struct {
+	RdapConformance []string `json:"rdapConformance"`
+	Notices         []struct {
+		Title       string   `json:"title"`
+		Description []string `json:"description"`
+		Links       []struct {
+			Value string `json:"value"`
+			Rel   string `json:"rel"`
+			Type  string `json:"type"`
+			Href  string `json:"href"`
+		} `json:"links,omitempty"`
+	} `json:"notices"`
+	Handle       string `json:"handle"`
+	StartAddress string `json:"startAddress"`
+	EndAddress   string `json:"endAddress"`
+	IPVersion    string `json:"ipVersion"`
+	Name         string `json:"name"`
+	Type         string `json:"type"`
+	ParentHandle string `json:"parentHandle"`
+	Events       []struct {
+		EventAction string `json:"eventAction"`
+		EventDate   string `json:"eventDate"`
+	} `json:"events"`
+	Links []struct {
+		Value string `json:"value"`
+		Rel   string `json:"rel"`
+		Type  string `json:"type"`
+		Href  string `json:"href"`
+	} `json:"links"`
+	Entities []struct {
+		Handle     string        `json:"handle"`
+		VcardArray []interface{} `json:"vcardArray"`
+		Roles      []string      `json:"roles"`
+		Remarks    []struct {
+			Title       string   `json:"title"`
+			Description []string `json:"description"`
+		} `json:"remarks"`
+		Links []struct {
+			Value string `json:"value"`
+			Rel   string `json:"rel"`
+			Type  string `json:"type"`
+			Href  string `json:"href"`
+		} `json:"links"`
+		Events []struct {
+			EventAction string `json:"eventAction"`
+			EventDate   string `json:"eventDate"`
+		} `json:"events"`
+		Entities []struct {
+			Handle     string        `json:"handle"`
+			VcardArray []interface{} `json:"vcardArray"`
+			Roles      []string      `json:"roles"`
+			Remarks    []struct {
+				Title       string   `json:"title"`
+				Description []string `json:"description"`
+			} `json:"remarks,omitempty"`
+			Links []struct {
+				Value string `json:"value"`
+				Rel   string `json:"rel"`
+				Type  string `json:"type"`
+				Href  string `json:"href"`
+			} `json:"links"`
+			Events []struct {
+				EventAction string `json:"eventAction"`
+				EventDate   string `json:"eventDate"`
+			} `json:"events"`
+			Port43          string   `json:"port43"`
+			ObjectClassName string   `json:"objectClassName"`
+			Status          []string `json:"status,omitempty"`
+		} `json:"entities"`
+		Port43          string `json:"port43"`
+		ObjectClassName string `json:"objectClassName"`
+	} `json:"entities"`
+	Port43          string   `json:"port43"`
+	Status          []string `json:"status"`
+	ObjectClassName string   `json:"objectClassName"`
+	Cidr0Cidrs      []struct {
+		V4Prefix string `json:"v4prefix"`
+		Length   int    `json:"length"`
+	} `json:"cidr0_cidrs"`
+	ArinOriginas0Originautnums []interface{} `json:"arin_originas0_originautnums"`
+}
+
 func main() {
-	http.HandleFunc("/", ExampleHandler)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		panic(err)
-	}
-}
-
-func ExampleHandler(w http.ResponseWriter, r *http.Request) {
-	niler := func(v string) interface{} {
-		if v == "" {
-			return "null"
-		}
-		return v
-	}
-	w.Header().Add("Content-Type", "application/json")
-
-	hostname := GetHostName(r.Header.Get("X-Forwarded-For"))
-	reverseIp := GetReverseIp(hostname)
-
-	resp, _ := json.MarshalIndent(map[string]interface{}{
-		"ip":                        niler(r.Header.Get("X-Forwarded-For")),
-		"hostname":                  niler(hostname),
-		"reverse_hostname":          niler(reverseIp),
-		"user_agent":                niler(r.Header.Get("User-Agent")),
-	}, "", "  ")
-
-	w.Write(resp)
-}
-
-func GetHostName(ip string) string {
-	host, err := net.LookupAddr(ip)
+	//logger
+	err := initLogger()
 	if err != nil {
-		return ""
+		log.Fatal(err.Error())
 	}
-	return host[0]
+	//router
+	r := http.NewServeMux()
+	//routes
+	r.HandleFunc("/", reverseIpHandler)
+	r.HandleFunc("/ip/", ipHandler)
+	//http server
+	log.Println("api started...")
+	err = http.ListenAndServe(":8080", r)
+	log.Fatal(err.Error())
 }
 
-func GetReverseIp(host string) string {
-	addr, err := net.LookupIP(host)
-	if err == nil {
-		return addr[0].String()
+func initLogger() error {
+	//output (fileMode: -rw-r--r--)
+	lf, err := os.OpenFile("logs.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Printf("debug: %s\n", err.Error())
+		return errors.New("initializing logger failed!")
 	}
-	return ""
+	log.SetOutput(lf)
+	//flags
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	return nil
+}
+
+func reverseIpHandler(w http.ResponseWriter, r *http.Request) {
+	//reverse ip
+	rip := getReverseIp(r)
+	returnWhoisData(rip, w)
+}
+
+func ipHandler(w http.ResponseWriter, r *http.Request) {
+	//ip
+	ip, err := getIpParam(r)
+	if err != nil {
+		log.Printf("debug: %s\n", err.Error())
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+	returnWhoisData(ip, w)
+}
+
+func returnWhoisData(ip string, w http.ResponseWriter) {
+	//whois data
+	c := http.Client{
+		Timeout: time.Duration(time.Second * 30),
+	}
+	url := fmt.Sprintf("https://rdap.arin.net/registry/ip/%s.json", ip)
+	r, err := c.Get(url)
+	if err != nil {
+		log.Printf("debug: %s\n", err.Error())
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	defer r.Body.Close()
+	var wd ArinRdapData
+	err = json.NewDecoder(r.Body).Decode(&wd)
+	if err != nil {
+		log.Printf("debug: %s\n", err.Error())
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	//response
+	err = json.NewEncoder(w).Encode(&wd)
+	if err != nil {
+		log.Printf("debug: %s\n", err.Error())
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+}
+
+func getIpParam(r *http.Request) (string, error) {
+	//url path: /ip/:ip
+	ip := r.URL.Path[4:]
+	i := strings.Index(ip, "/")
+	if i == -1 {
+		return ip, nil
+	} else {
+		return "", errors.New("getting ip parameter failed!")
+	}
+}
+
+func getReverseIp(r *http.Request) string {
+	ff := r.Header.Get("X-FORWARDED-FOR")
+	if ff != "" {
+		return ff
+	}
+	//fall back to request's remote address
+	ip := getIp(r.RemoteAddr)
+	return ip
+}
+
+func getIp(remoteAddress string) string {
+	i := strings.Index(remoteAddress, ":")
+	return remoteAddress[0:i]
 }
