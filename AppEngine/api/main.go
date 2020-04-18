ef3919d29cd55293fc36124986e5a6b78dc32fc4
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-04-17 22:50:46 -0400

    Update main.go

--- AppEngine/api/main.go
+++ AppEngine/api/main.go
@@ -4,16 +4,47 @@ import (
 	"encoding/json"
 	"net"
 	"net/http"
+	"encoding/xml"
 )
 
 func main() {
 	http.HandleFunc("/", ExampleHandler)
+	http.HandleFunc("/xml", XmlHandler)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
 }
 
 func ExampleHandler(w http.ResponseWriter, r *http.Request) {
+	niler := func(v string) interface{} {
+		if v == "" {
+			return "null"
+		}
+		return v
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.Header().Add("Content-Security-Policy", "script-src 'self'; object-src 'self'")
+	w.Header().Add("Referrer-Policy", "strict-origin")
+	w.Header().Add("Strict-Transport-Security", "max-age=63072000; includeSubDomains; preload")
+	w.Header().Add("Feature-Policy", "vibrate 'self'")
+	w.Header().Add("X-Frame-Options", "SAMEORIGIN")
+	w.Header().Add("X-Content-Type-Options", "nosniff")
+
+	hostname := GetHostName(r.Header.Get("CF-Connecting-IP"))
+	reverseIp := GetReverseIp(hostname)
+
+	resp, _ := json.Marshal(map[string]interface{}{
+		"success":   niler("true"),
+		"ip":        niler(r.Header.Get("CF-Connecting-IP")),
+		"hostname":  niler(hostname),
+		"reverse":   niler(reverseIp),
+		"useragent": niler(r.Header.Get("User-Agent")),
+	})
+
+	w.Write(resp)
+}
+
+func XmlHandler(w http.ResponseWriter, r *http.Request) {
 	niler := func(v string) interface{} {
 		if v == "" {
 			return "null"
