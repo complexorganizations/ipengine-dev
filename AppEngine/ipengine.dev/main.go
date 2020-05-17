0e1261bf639c034358ef718fe7a6ef16cfe727b5
Prajwal Koirala <prajwalkoirala23@protonmail.com>
2020-05-17 01:59:54 -0400

    theme

--- AppEngine/ipengine.dev/main.go
+++ AppEngine/ipengine.dev/main.go
@@ -3,6 +3,6 @@ package main
 import "net/http"
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./www")))
+	http.Handle("/", http.FileServer(http.Dir("./web")))
 	http.ListenAndServe(":8080", nil)
 }
