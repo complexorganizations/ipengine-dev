d889a19cc97fa45f5b34e38c79dfe3dabf1dc79b
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-07-20 19:58:50 -0400

    Update main.go

--- main.go
+++ main.go
@@ -3,57 +3,68 @@ package main
 import (
 	"bufio"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"strings"
 )
 
-func main() {
-	files, err := ioutil.ReadDir(".")
-	if err != nil {
-		log.Fatal(err)
-	}
+var arr = []string{"."}
 
+func main() {
 	var mainJSON map[string][]string = make(map[string][]string)
 
-	for _, f := range files {
-		fName := f.Name()
-		fmt.Println(fName)
-		res1 := strings.Index(fName, ".")
-		fmt.Println(res1)
-		if res1 != -1 && (fName[res1:] == ".ipset" || fName[res1:] == ".netstat") {
-			file, err := os.Open(fName)
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer file.Close()
+	for a := 0; a < len(arr); a++ {
 
-			mainCat := ""
-			scanner := bufio.NewScanner(file)
-			for scanner.Scan() {
-				txt := scanner.Text()
-				catlen := len("# Category")
-				if txt[0] != []byte("#")[0] {
-					// fmt.Println(scanner.Text())
-					mainJSON[mainCat] = append(mainJSON[mainCat], scanner.Text())
-				} else if len(txt) >= catlen && txt[:catlen] == "# Category" {
-					cat := txt[catlen:]
-					catTrim := strings.Trim(cat, " ")
-					if catTrim[0] == []byte(":")[0] && len(catTrim) > 2 {
-						catTrim = catTrim[2:]
-						_, ok := mainJSON[catTrim]
-						if !ok {
-							mainJSON[catTrim] = []string{}
-						}
-						mainCat = catTrim
-					}
+		files, err := ioutil.ReadDir(arr[a])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, f := range files {
+			fName := f.Name()
+			fi, err := os.Stat(arr[a] + "/" + fName)
+			if err == nil {
+				mode := fi.Mode()
+
+				if mode.IsDir() {
+					arr = append(arr, fName)
+					continue
 				}
 			}
+			res1 := strings.Index(fName, ".")
+			if res1 != -1 && (fName[res1:] == ".ipset" || fName[res1:] == ".netstat") {
+				file, err := os.Open(arr[a] + "/" + fName)
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer file.Close()
 
-			if err := scanner.Err(); err != nil {
-				log.Fatal(err)
+				mainCat := ""
+				scanner := bufio.NewScanner(file)
+				for scanner.Scan() {
+					txt := scanner.Text()
+					catlen := len("# Category")
+					if txt[0] != []byte("#")[0] {
+						// fmt.Println(scanner.Text())
+						mainJSON[mainCat] = append(mainJSON[mainCat], scanner.Text())
+					} else if len(txt) >= catlen && txt[:catlen] == "# Category" {
+						cat := txt[catlen:]
+						catTrim := strings.Trim(cat, " ")
+						if catTrim[0] == []byte(":")[0] && len(catTrim) > 2 {
+							catTrim = catTrim[2:]
+							_, ok := mainJSON[catTrim]
+							if !ok {
+								mainJSON[catTrim] = []string{}
+							}
+							mainCat = catTrim
+						}
+					}
+				}
+
+				if err := scanner.Err(); err != nil {
+					log.Fatal(err)
+				}
 			}
 		}
 	}
