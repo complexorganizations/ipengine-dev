e2af095d8b6e4dda2a2ac44dfdec9f8aed88aefc
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-07-20 09:07:13 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,63 @@
 package main
 
 import (
-        "fmt"  
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
 )
+
+func main() {
+	files, err := ioutil.ReadDir(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var mainJSON map[string][]string = make(map[string][]string)
+
+	for _, f := range files {
+		fName := f.Name()
+		fmt.Println(fName)
+		res1 := strings.Index(fName, ".")
+		fmt.Println(res1)
+		if res1 != -1 && fName[res1:] == ".ipset" {
+			file, err := os.Open(fName)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer file.Close()
+
+			mainCat := ""
+			scanner := bufio.NewScanner(file)
+			for scanner.Scan() {
+				txt := scanner.Text()
+				catlen := len("# Category")
+				if txt[0] != []byte("#")[0] {
+					// fmt.Println(scanner.Text())
+					mainJSON[mainCat] = append(mainJSON[mainCat], scanner.Text())
+				} else if len(txt) >= catlen && txt[:catlen] == "# Category" {
+					cat := txt[catlen:]
+					catTrim := strings.Trim(cat, " ")
+					if catTrim[0] == []byte(":")[0] && len(catTrim) > 2 {
+						catTrim = catTrim[2:]
+						_, ok := mainJSON[catTrim]
+						if !ok {
+							mainJSON[catTrim] = []string{}
+						}
+						mainCat = catTrim
+					}
+				}
+			}
+
+			if err := scanner.Err(); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+	d1, _ := json.Marshal(mainJSON)
+	ioutil.WriteFile("output.json", d1, 0644)
+	// fmt.Println(json.Marshal(mainJSON))
+}
