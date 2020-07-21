1535c15032dab7f4e123160fd76dc1cde3623d6e
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-07-21 02:16:21 -0400

    Update update-blocklist-ipsets.sh

--- update-blocklist-ipsets.sh
+++ update-blocklist-ipsets.sh
@@ -36,7 +36,7 @@ function update-blocklist-ipsets() {
         git clone https://github.com/firehol/blocklist-ipsets.git
         cp main.go blocklist-ipsets/main.go
         cd blocklist-ipsets
-        mv geolite2_country/* ../ && mv ip2location_country/* ../ && mv ipdeny_country ../ && mv ipip_country ../
+        mv geolite2_country/* ../blocklist-ipsets/ && mv ip2location_country/* ../blocklist-ipsets/ && mv ipdeny_country ../blocklist-ipsets/ && mv ipip_country ../blocklist-ipsets/
         rm -rf geolite2_country && rm -rf ip2location_country && rm -rf ipdeny_country && rm -rf ipip_country
         go run main.go
         mv output.json ../AppEngine/api.ipengine.dev/output.json
