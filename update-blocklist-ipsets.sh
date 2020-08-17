e6e27e2961541ba2f6d95f827a8b654701ee98b6
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-08-16 23:38:55 -0400

    Update update-blocklist-ipsets.sh

--- update-blocklist-ipsets.sh
+++ update-blocklist-ipsets.sh
@@ -34,10 +34,9 @@ function update-blocklist-ipsets() {
     # Update begins here
     if ([ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ]); then
         git clone https://github.com/firehol/blocklist-ipsets.git
-        cp main.go blocklist-ipsets/main.go
         mv blocklist-ipsets/geolite2_country/* blocklist-ipsets/ && mv blocklist-ipsets/ip2location_country/* blocklist-ipsets/ && mv blocklist-ipsets/ipdeny_country/* blocklist-ipsets/ && mv blocklist-ipsets/ipip_country/* blocklist-ipsets/
         rm -rf blocklist-ipsets/geolite2_country && rm -rf blocklist-ipsets/ip2location_country && rm -rf blocklist-ipsets/ipdeny_country && rm -rf blocklist-ipsets/ipip_country
-        cd blocklist-ipsets && go run main.go && cd ../
+        cp main.go blocklist-ipsets/main.go && cd blocklist-ipsets && go run main.go && cd ../
         mv blocklist-ipsets/output.json AppEngine/api.ipengine.dev/output.json
         rm -rf blocklist-ipsets
     fi
