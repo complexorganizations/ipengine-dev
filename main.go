130d73f284b1bff4e4eeebe0da3f62ae0b8bf55d
Ricardo Gonçalves <ricardo.341928@hotmail.com>
2020-08-19 21:16:06 +0100

    fix something, want to fix more, will work on analytics

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -16,48 +15,53 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var mainJSON map[string][]string = make(map[string][]string)
+	mainJSON := make(map[string][]string)
 
 	for _, f := range files {
 		fName := f.Name()
-		fmt.Println(fName)
-		res1 := strings.Index(fName, ".")
-		fmt.Println(res1)
-		if res1 != -1 && (fName[res1:] == ".ipset" || fName[res1:] == ".netstat") {
-			file, err := os.Open(fName)
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer file.Close()
 
-			mainCat := ""
-			scanner := bufio.NewScanner(file)
-			for scanner.Scan() {
-				txt := scanner.Text()
-				catlen := len("# Category")
-				if txt[0] != []byte("#")[0] {
-					// fmt.Println(scanner.Text())
-					mainJSON[mainCat] = append(mainJSON[mainCat], scanner.Text())
-				} else if len(txt) >= catlen && txt[:catlen] == "# Category" {
-					cat := txt[catlen:]
-					catTrim := strings.Trim(cat, " ")
-					if catTrim[0] == []byte(":")[0] && len(catTrim) > 2 {
-						catTrim = catTrim[2:]
-						_, ok := mainJSON[catTrim]
-						if !ok {
-							mainJSON[catTrim] = []string{}
-						}
-						mainCat = catTrim
+		if res := strings.Index(fName, "."); res == -1 || (fName[res:] != ".ipset" && fName[res:] != ".netstat") {
+			continue
+		}
+
+		file, err := os.Open(fName)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		mainCat := ""
+		scanner := bufio.NewScanner(file)
+
+		for scanner.Scan() {
+			txt := scanner.Text()
+			catlen := len("# Category")
+
+			if txt[0] != []byte("#")[0] {
+				mainJSON[mainCat] = append(mainJSON[mainCat], scanner.Text())
+				continue
+			}
+
+			if len(txt) >= catlen && txt[:catlen] == "# Category" {
+				cat := txt[catlen:]
+				catTrim := strings.Trim(cat, " ")
+				if catTrim[0] == []byte(":")[0] && len(catTrim) > 2 {
+					catTrim = catTrim[2:]
+					_, ok := mainJSON[catTrim]
+					if !ok {
+						mainJSON[catTrim] = []string{}
 					}
+					mainCat = catTrim
 				}
 			}
-
-			if err := scanner.Err(); err != nil {
-				log.Fatal(err)
-			}
 		}
+
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+
+		_ = file.Close()
 	}
-	d1, _ := json.Marshal(mainJSON)
-	ioutil.WriteFile("blockips.json", d1, 0644)
-	// fmt.Println(json.Marshal(mainJSON))
+
+	data, _ := json.Marshal(mainJSON)
+	_ = ioutil.WriteFile("blockips.json", data, 0644)
 }
