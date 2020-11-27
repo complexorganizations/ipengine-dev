f5d55ba50089119201674e5f55304129d2aa769c
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-11-26 21:41:32 -0500

    Update update-blocklist-ipsets.sh

--- update-blocklist-ipsets.sh
+++ update-blocklist-ipsets.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-# Pre-Checks
-function check-system-requirements() {
-    # System requirements (git)
-    if ! [ -x "$(command -v git)" ]; then
-        echo "Error: git is not installed, please install git." >&2
-    fi
-    # System requirements (go)
-    if ! [ -x "$(command -v go)" ]; then
-        echo "Error: go is not installed, please install go." >&2
-    fi
-}
-
-# Run the function and check for requirements
-check-system-requirements
-
 # Detect Operating System
 function dist-check() {
     if [ -e /etc/os-release ]; then
@@ -26,10 +11,25 @@ function dist-check() {
 # Check Operating System
 dist-check
 
+# Pre-Checks
+function check-system-requirements() {
+    # System requirements (git)
+    if ! [ -x "$(command -v git)" ]; then
+        echo "Error: git is not installed, please install git." >&2
+    fi
+    # System requirements (go)
+    if ! [ -x "$(command -v go)" ]; then
+        echo "Error: go is not installed, please install go." >&2
+    fi
+        apt-get install golang-go git -y
+}
+
+# Run the function and check for requirements
+check-system-requirements
+
 # Update the blocklist ipset
 function update-blocklist-ipsets() {
     if ([ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ]); then
-        apt-get install golang-go git -y
         git clone https://github.com/firehol/blocklist-ipsets.git
         mv blocklist-ipsets/geolite2_country/* blocklist-ipsets/ && mv blocklist-ipsets/ip2location_country/* blocklist-ipsets/ && mv blocklist-ipsets/ipdeny_country/* blocklist-ipsets/ && mv blocklist-ipsets/ipip_country/* blocklist-ipsets/
         rm -rf blocklist-ipsets/geolite2_country && rm -rf blocklist-ipsets/ip2location_country && rm -rf blocklist-ipsets/ipdeny_country && rm -rf blocklist-ipsets/ipip_country
