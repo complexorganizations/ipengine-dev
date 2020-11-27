ae6ccfd258e971386d135d610b49413259ed017e
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-11-27 15:19:00 -0500

    Update main.go

--- main.go
+++ main.go
@@ -12,35 +12,27 @@ import (
 func main() {
 	files, err := ioutil.ReadDir(".")
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
-
 	mainJSON := make(map[string][]string)
-
 	for _, f := range files {
 		fName := f.Name()
-
 		if res := strings.Index(fName, "."); res == -1 || (fName[res:] != ".ipset" && fName[res:] != ".netstat") {
 			continue
 		}
-
 		file, err := os.Open(fName)
 		if err != nil {
-			log.Fatal(err)
+			log.Println(err)
 		}
-
 		mainCat := ""
 		scanner := bufio.NewScanner(file)
-
 		for scanner.Scan() {
 			txt := scanner.Text()
 			catlen := len("# Category")
-
 			if txt[0] != []byte("#")[0] {
 				mainJSON[mainCat] = append(mainJSON[mainCat], scanner.Text())
 				continue
 			}
-
 			if len(txt) >= catlen && txt[:catlen] == "# Category" {
 				cat := txt[catlen:]
 				catTrim := strings.Trim(cat, " ")
@@ -54,14 +46,11 @@ func main() {
 				}
 			}
 		}
-
 		if err := scanner.Err(); err != nil {
-			log.Fatal(err)
+			log.Println(err)
 		}
-
 		_ = file.Close()
 	}
-
 	data, _ := json.Marshal(mainJSON)
 	_ = ioutil.WriteFile("blockips.json", data, 0644)
 }
