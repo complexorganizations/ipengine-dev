4770b7b479fbe81a184cbcb4db4f5cb4b1fc2285
Prajwal Koirala <prajwalkoirala23@protonmail.com>
2020-12-02 09:15:28 -0500

    update

--- scripts/update-blocklist-ipsets.sh
+++ scripts/update-blocklist-ipsets.sh
@@ -21,7 +21,6 @@ function check-system-requirements() {
     if ! [ -x "$(command -v go)" ]; then
         echo "Error: go is not installed, please install go." >&2
     fi
-        apt-get install golang-go git -y
 }
 
 # Run the function and check for requirements
@@ -30,10 +29,11 @@ check-system-requirements
 # Update the blocklist ipset
 function update-blocklist-ipsets() {
     if ([ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ]); then
+        apt-get install golang-go git -y
         git clone https://github.com/firehol/blocklist-ipsets.git
         mv blocklist-ipsets/geolite2_country/* blocklist-ipsets/ && mv blocklist-ipsets/ip2location_country/* blocklist-ipsets/ && mv blocklist-ipsets/ipdeny_country/* blocklist-ipsets/ && mv blocklist-ipsets/ipip_country/* blocklist-ipsets/
         rm -rf blocklist-ipsets/geolite2_country && rm -rf blocklist-ipsets/ip2location_country && rm -rf blocklist-ipsets/ipdeny_country && rm -rf blocklist-ipsets/ipip_country
-        cp main.go blocklist-ipsets/main.go && cd blocklist-ipsets && go run main.go && cd ../
+        cp blockipsUpdate.go blocklist-ipsets/blockipsUpdate.go && cd blocklist-ipsets && go run blockipsUpdate.go && cd ../
         rm -f AppEngine/api.ipengine.dev/blockips.json
         mv blocklist-ipsets/blockips.json AppEngine/api.ipengine.dev/blockips.json
         rm -rf blocklist-ipsets
