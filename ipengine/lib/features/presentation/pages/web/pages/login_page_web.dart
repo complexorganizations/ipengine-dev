0a720cc911fb8c3058e265a18ed1de1e7a725b6f
Yuga Jain <yugaljain002@gmail.com>
2020-12-05 18:29:33 +0530

    Updated

--- ipengine/lib/features/presentation/pages/web/pages/login_page_web.dart
+++ ipengine/lib/features/presentation/pages/web/pages/login_page_web.dart
@@ -185,34 +185,6 @@ class _LoginPageWebState extends State<LoginPageWeb> {
     );
   }
 
-  Future<String> signInWithGoogle() async {
-    print('GMAIL+++ 1');
-    final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
-    final GoogleSignInAuthentication googleSignInAuthentication =
-    await googleSignInAccount.authentication;
 
-    final AuthCredential credential = GoogleAuthProvider.getCredential(
-      accessToken: googleSignInAuthentication.accessToken,
-      idToken: googleSignInAuthentication.idToken,
-    );
-
-    final AuthResult authResult = await _auth.signInWithCredential(credential);
-    final FirebaseUser user = authResult.user;
-
-    assert(!user.isAnonymous);
-    assert(await user.getIdToken() != null);
-
-    final FirebaseUser currentUser = await _auth.currentUser();
-    assert(user.uid == currentUser.uid);
-
-    print('GMAIL+++');
-    print(currentUser.uid);
-    return 'signInWithGoogle succeeded: $user';
-  }
-
-  void signOutGoogle() async{
-    await googleSignIn.signOut();
-    print("User Sign Out");
-  }
 
 }
