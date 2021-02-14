b52fa409339c20fd5653e99fc55e63e683c45d4b
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-02-14 15:07:06 -0500

    Update update-blocklist-ipsets.sh

--- scripts/update-blocklist-ipsets.sh
+++ scripts/update-blocklist-ipsets.sh
@@ -11,25 +11,34 @@ function dist-check() {
 # Check Operating System
 dist-check
 
-# Pre-Checks
-function check-system-requirements() {
-    # System requirements (git)
-    if ! [ -x "$(command -v git)" ]; then
-        echo "Error: git is not installed, please install git." >&2
+# Pre-Checks system requirements
+function installing-system-requirements() {
+  if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "pop" ] || [ "$DISTRO" == "kali" ] || [ "$DISTRO" == "linuxmint" ] || [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ] || [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ] || [ "$DISTRO" == "alpine" ] || [ "$DISTRO" == "freebsd" ]; }; then
+    if { [ ! -x "$(command -v git)" ] || [ ! -x "$(command -v go)" ]; }; then
+      if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "pop" ] || [ "$DISTRO" == "kali" ] || [ "$DISTRO" == "linuxmint" ]; }; then
+        apt-get update && apt-get install golang-go git -y
+      elif { [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ]; }; then
+        yum update -y && yum install golang-go git -y
+      elif { [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ]; }; then
+        pacman -Syu --noconfirm iptables golang-go git
+      elif [ "$DISTRO" == "alpine" ]; then
+        apk update && apk add iptables golang-go git
+      elif [ "$DISTRO" == "freebsd" ]; then
+        pkg update && pkg install golang-go git
+      fi
     fi
-    # System requirements (go)
-    if ! [ -x "$(command -v go)" ]; then
-        echo "Error: go is not installed, please install go." >&2
-    fi
+  else
+    echo "Error: $DISTRO not supported."
+    exit
+  fi
 }
 
 # Run the function and check for requirements
-check-system-requirements
+installing-system-requirements
 
 # Update the blocklist ipset
 function update-blocklist-ipsets() {
-    if ([ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ]); then
-        apt-get install golang-go git -y
+    if [ -x "$(command -v go)" ]; then
         git clone https://github.com/firehol/blocklist-ipsets.git
         mv blocklist-ipsets/geolite2_country/* blocklist-ipsets/ && mv blocklist-ipsets/ip2location_country/* blocklist-ipsets/ && mv blocklist-ipsets/ipdeny_country/* blocklist-ipsets/ && mv blocklist-ipsets/ipip_country/* blocklist-ipsets/
         rm -rf blocklist-ipsets/geolite2_country && rm -rf blocklist-ipsets/ip2location_country && rm -rf blocklist-ipsets/ipdeny_country && rm -rf blocklist-ipsets/ipip_country
@@ -37,6 +46,9 @@ function update-blocklist-ipsets() {
         rm -f AppEngine/api.ipengine.dev/blockips.json
         mv blocklist-ipsets/blockips.json AppEngine/api.ipengine.dev/blockips.json
         rm -rf blocklist-ipsets
+    else
+        echo "Error: In your system, Go wasn't found."
+        exit
     fi
 }
 
