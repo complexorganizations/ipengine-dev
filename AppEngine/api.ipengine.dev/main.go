9d37a03e92316f04d7ffc1cdfd925b0296f0bc35
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-08-14 22:45:58 -0400

    Update main.go

--- AppEngine/api.ipengine.dev/main.go
+++ AppEngine/api.ipengine.dev/main.go
@@ -53,8 +53,12 @@ func main() {
 
 // The substance of the response to write
 func personalRequestWriter(writer http.ResponseWriter, req *http.Request) {
-	writer.Header().Set("Content-Type", "application/json")
-	fmt.Fprintf(writer, "%s", jsonResponse(req))
+	if req.URL.Path == "/" {
+		writer.Header().Set("Content-Type", "application/json")
+		fmt.Fprintf(writer, "%s", jsonResponse(req))
+	} else {
+		http.HandleFunc(req.URL.Path, handleAllErrors)
+	}
 }
 
 func jsonResponse(httpRequest *http.Request) []byte {
@@ -242,4 +246,36 @@ func isInBlackList(ip string, blacklistType string) bool {
 		}
 	}
 	return false
+}
+
+func handleAllErrors(httpWriter http.ResponseWriter, r *http.Request) {
+	// Set the header to status not found.
+	httpWriter.WriteHeader(http.StatusNotFound)
+	// Set the content type to application/json.
+	httpWriter.Header().Set("Content-Type", "application/json")
+	// Set the body to an error message.
+	type errorMessage struct {
+		Code    int
+		Message string
+	}
+	errorMsg := errorMessage{
+		Code:    http.StatusNotFound,
+		Message: "Resource not found",
+	}
+	// Wrap the error in a error object.
+	type jsonError struct {
+		Error errorMessage
+	}
+	// The content of the error object.
+	jsonReturn := jsonError{
+		Error: errorMsg,
+	}
+	// Marshal the error message to JSON.
+	errorJsonMessage, err := json.Marshal(jsonReturn)
+	// Log the error if there is one.
+	if err != nil {
+		log.Println(err)
+	}
+	// Write the JSON error message.
+	httpWriter.Write(errorJsonMessage)
 }
